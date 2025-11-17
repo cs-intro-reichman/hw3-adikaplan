d828c6bc5b8daff21a5cc1a04706fb8fe1941e94
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-18 00:09:11 +0200

    completed task

--- Anagram.java
+++ Anagram.java
@@ -76,7 +76,7 @@ public class Anagram {
 		for(int i = str.length() ;  i > 0; i--){
 			int indexChar =(int)(Math.random()* str.length() +1);
 			strRandom = strRandom + copyStr.charAt(indexChar);
-			copyStr = copyStr.substring(0,indexChar) + copyStr.substring(indexChar);
+			copyStr = copyStr.substring(0,indexChar) + copyStr.substring(indexChar + 1);
 		}
 		return strRandom;
 	}
