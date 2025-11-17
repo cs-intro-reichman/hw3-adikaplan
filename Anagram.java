88011c0695bb8905f5cf7a5a77736411164fa86c
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-17 11:15:03 +0200

    completed task 3

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,56 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		if (str1.length() != str2.length()){
+			return false;
+		} 
+		String copyStr = str2;
+		for(int i =0; i< str1.length() ; i++){
+			char char1 = str1.charAt(i);
+			int match = -1;
+			for(int j = 0; j < copyStr.length(); j++){
+				if(char1 == copyStr.charAt(j)){
+					match = j;
+					break;
+				}
+			}
+			if(match != -1){
+				copyStr = copyStr.substring(0,match) + copyStr.substring(match +1);
+			}else{
+				return false;
+			}
 	}
+	    return true;
+}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+	public static String preProcess(String str){
+		str = str.toLowerCase();
+		String Alphabet = "abcdefghijklmnopqrstuvwxyz";
+		String newString = "";
+		for(int i =0; i< str.length(); i++){
+			char charecter = str.charAt(i);
+			if(Alphabet.indexOf(charecter)>=0){
+				newString = newString + charecter;
+			} 
+		}
+		return newString;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String strRandom = "";
+		String copyStr = str;
+		for(int i = str.length() ;  i > 0; i--){
+			int indexChar =(int)(Math.random()* str.length());
+			strRandom = strRandom + copyStr.charAt(indexChar);
+			copyStr = copyStr.substring(0,indexChar) + copyStr.substring(indexChar);
+		}
+		return strRandom;
 	}
 }
